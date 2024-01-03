52aba7b92fb1e6dfc6aa631b7681154c6d63acfb
Artem Berdnikov <berdnikovartem775@gmail.com>
2024-01-03 12:37:57 +0500

    final version

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 async function handleRegister() {
-  const username = document.querySelector(
-    "div[name=registration] input[name=login]"
-  ).value;
-  const password = document.querySelector(
-    "div[name=registration] input[name=password]"
-  ).value;
+  const username = document.querySelector(".input[name=login]").value;
+  const password = document.querySelector(".input[name=password]").value;
   const response = await fetch(
     "https://testbackend-1-m4859670.deta.app/register",
     {
@@ -26,10 +22,10 @@ async function handleRegister() {
 
 async function handleLogin() {
   const username = document.querySelector(
-    "div[name=authorization] input[name=login]"
+    "div[name=authorization] input[name=loginEntry]"
   ).value;
   const password = document.querySelector(
-    "div[name=authorization] input[name=password]"
+    "div[name=authorization] input[name=passwordEntry]"
   ).value;
   const response = await fetch(
     "https://testbackend-1-m4859670.deta.app/login",
